5ca94f7dbf66a12623db34312fa2d98cfad446ba
Steven Mathew <mathewsteven92@gmail.com>
2025-07-23 17:03:49 -0400

    Updated Python ile

--- app.py
+++ app.py
@@ -4,29 +4,33 @@ import os
 import random
 import argparse
 
-
 app = Flask(__name__)
 
-DBHOST = os.environ.get("DBHOST") or "localhost"
-DBUSER = os.environ.get("DBUSER") or "root"
-DBPWD = os.environ.get("DBPWD") or "passwors"
-DATABASE = os.environ.get("DATABASE") or "employees"
-COLOR_FROM_ENV = os.environ.get('APP_COLOR') or "lime"
-DBPORT = int(os.environ.get("DBPORT"))
+# Database config from secrets or fallback
+DBHOST = os.environ.get("DBHOST", "localhost")
+DBUSER = os.environ.get("DBUSER", "root")
+DBPWD = os.environ.get("DBPWD", "password")
+DATABASE = os.environ.get("DATABASE", "employees")
+DBPORT = int(os.environ.get("DBPORT", 3306))
 
-# Create a connection to the MySQL database
+# Customizations from ConfigMap
+COLOR_FROM_ENV = os.environ.get("APP_COLOR", "lime")
+BG_IMAGE_URL = os.environ.get("BG_IMAGE_URL", "/static/default.jpg")
+MY_NAME = os.environ.get("MY_NAME", "Your Name")
+
+# Print background image to logs (required)
+print(f"Background image URL: {BG_IMAGE_URL}")
+
+# Create MySQL connection
 db_conn = connections.Connection(
-    host= DBHOST,
+    host=DBHOST,
     port=DBPORT,
-    user= DBUSER,
-    password= DBPWD, 
-    db= DATABASE
-    
+    user=DBUSER,
+    password=DBPWD,
+    db=DATABASE
 )
-output = {}
-table = 'employee';
 
-# Define the supported color codes
+# Supported color codes
 color_codes = {
     "red": "#e74c3c",
     "green": "#16a085",
@@ -37,22 +41,17 @@ color_codes = {
     "lime": "#C1FF9C",
 }
 
-
-# Create a string of supported colors
 SUPPORTED_COLORS = ",".join(color_codes.keys())
-
-# Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink", "lime"])
-
+COLOR = COLOR_FROM_ENV if COLOR_FROM_ENV in color_codes else random.choice(list(color_codes.keys()))
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('addemp.html', color=color_codes[COLOR])
+    return render_template("addemp.html", color=color_codes[COLOR], background=BG_IMAGE_URL, name=MY_NAME)
 
-@app.route("/about", methods=['GET','POST'])
+@app.route("/about", methods=['GET', 'POST'])
 def about():
-    return render_template('about.html', color=color_codes[COLOR])
-    
+    return render_template("about.html", color=color_codes[COLOR], background=BG_IMAGE_URL, name=MY_NAME)
+
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
     emp_id = request.form['emp_id']
@@ -61,76 +60,50 @@ def AddEmp():
     primary_skill = request.form['primary_skill']
     location = request.form['location']
 
-  
     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
-
     try:
-        
-        cursor.execute(insert_sql,(emp_id, first_name, last_name, primary_skill, location))
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, primary_skill, location))
         db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
-
+        emp_name = f"{first_name} {last_name}"
     finally:
         cursor.close()
 
-    print("all modification done...")
-    return render_template('addempoutput.html', name=emp_name, color=color_codes[COLOR])
+    return render_template("addempoutput.html", name=emp_name, color=color_codes[COLOR], background=BG_IMAGE_URL, name_header=MY_NAME)
 
 @app.route("/getemp", methods=['GET', 'POST'])
 def GetEmp():
-    return render_template("getemp.html", color=color_codes[COLOR])
+    return render_template("getemp.html", color=color_codes[COLOR], background=BG_IMAGE_URL, name=MY_NAME)
 
-
-@app.route("/fetchdata", methods=['GET','POST'])
+@app.route("/fetchdata", methods=['GET', 'POST'])
 def FetchData():
     emp_id = request.form['emp_id']
-
     output = {}
-    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location from employee where emp_id=%s"
+    select_sql = "SELECT emp_id, first_name, last_name, primary_skill, location FROM employee WHERE emp_id=%s"
     cursor = db_conn.cursor()
-
     try:
-        cursor.execute(select_sql,(emp_id))
+        cursor.execute(select_sql, (emp_id))
         result = cursor.fetchone()
-        
-        # Add No Employee found form
-        output["emp_id"] = result[0]
-        output["first_name"] = result[1]
-        output["last_name"] = result[2]
-        output["primary_skills"] = result[3]
-        output["location"] = result[4]
-        
+        if result:
+            output["emp_id"] = result[0]
+            output["first_name"] = result[1]
+            output["last_name"] = result[2]
+            output["primary_skills"] = result[3]
+            output["location"] = result[4]
     except Exception as e:
         print(e)
-
     finally:
         cursor.close()
 
-    return render_template("getempoutput.html", id=output["emp_id"], fname=output["first_name"],
-                           lname=output["last_name"], interest=output["primary_skills"], location=output["location"], color=color_codes[COLOR])
+    return render_template("getempoutput.html",
+                           id=output.get("emp_id", ""),
+                           fname=output.get("first_name", ""),
+                           lname=output.get("last_name", ""),
+                           interest=output.get("primary_skills", ""),
+                           location=output.get("location", ""),
+                           color=color_codes[COLOR],
+                           background=BG_IMAGE_URL,
+                           name=MY_NAME)
 
-if __name__ == '__main__':
-    
-    # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
-    else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
-    app.run(host='0.0.0.0',port=8080,debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=81, debug=True)
